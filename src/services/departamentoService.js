8ec64d28bb52f5627f9ed9686227e2b077048d7f
Santiago Lopez Roth <santiago_97_07@hotmail.com>
2021-07-06 20:28:18 -0300

    Modificando endpoint 'getBySearch' departamento para las nuevas restricciones

--- src/services/departamentoService.js
+++ src/services/departamentoService.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import { REST_SERVER_URL } from './configuration'
 import { Departamento } from '../domain/departamento'
+import { usuarioService } from '../services/usuarioService';
 
 class DepartamentoService {
 
     async getBySearch(palabraBuscada) {
-        const listaJSON = await axios.get(`${REST_SERVER_URL}/departamentos`, {params:{ palabraBuscada }})
+        const idLogueado = usuarioService.usuarioLogueado.id
+        const listaJSON = await axios.get(`${REST_SERVER_URL}/departamentos`, {params:{ palabraBuscada, idLogueado }})
         return listaJSON.data
     }
     
