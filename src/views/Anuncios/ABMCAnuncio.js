36b73b523d2e9b04a22cd5ed7297c42c8c7f5354
Naahueeh <nahuelramos518@gmail.com>
2021-07-29 23:07:15 -0300

    Enviar mail geenral cuando hay un anuncio nuevo

--- src/views/Anuncios/ABMCAnuncio.js
+++ src/views/Anuncios/ABMCAnuncio.js
@@ -139,6 +139,11 @@ export const ABMCAnuncio = ({ edicion, creacion }) => {
         try {
             if (validarAnuncio()) {
                 await anuncioService.create(anuncio)
+                try{
+                    anuncioService.mandarCorreoNuevoAnuncio(anuncio.titulo)
+                } catch (error) {
+                   window.alert("No se pudo enviar el correo a todo el consorcio notificando el nuevo anuncio")
+                }                
                 setCampoEditado(false)
                 history.push("/anuncios", { openChildSnack: true, mensajeChild: "Anuncio creado correctamente." })
             } else {
